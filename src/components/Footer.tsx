25b84f3000d0f36d2f9aa8a0460978beb9bf7fc5
Bedasa Negash <bdhsane@gmail.com>
2026-08-05 17:45:19 +0300

    fix testimonals

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 // components/Footer.tsx
-import { FaGithub, FaLinkedin, FaEnvelope, FaTelegramPlane, FaFacebook } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaEnvelope, FaTelegramPlane } from "react-icons/fa";
 
 export default function Footer() {
   return (
